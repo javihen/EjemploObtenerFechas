2cab307ded5aeffe5baf275a3adf0e21f3ec05c2
javier <javi_hen@hotmail.com>
2020-09-30 12:26:34 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploFechas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaramos una variable de tipo cadena o palabra
+            string dia = "";
+            //Obtenemos la fecha de la computadora
+            DateTime fecha = DateTime.Parse(DateTime.Now.ToShortDateString());
+            //Obtenemos solo el dia de la semana de la fecha de la computadora
+            dia = fecha.ToString("dddd");
+            //mostramos la fecha completa y el dia de la computadora
+            Console.WriteLine(fecha);
+            Console.WriteLine(dia);
+            //declaramos una variable de tipo numero real con decimales
+            double costo = 0;
+            //preguntamos en un switch que dia es hoy y de esta manera darle el valor del la variable costo
+            switch (dia)
+            {
+                case "domingo": costo = 2;break;
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves": costo = 4; break;
+                case "viernes":
+                case "sabado":costo = 7; break;
+            }
+            //mostramos el valor de costo, tomando en cuenta el dia de la semana
+            Console.WriteLine(costo);
+            Console.ReadKey();
+        }
+    }
+}
