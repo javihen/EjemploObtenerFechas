1ee9a5330dac661fc7116d692792c4f2bdd689fc
javier <javi_hen@hotmail.com>
2020-09-30 12:35:01 -0400

    CAmbio en el acento del dia

--- Program.cs
+++ Program.cs
@@ -22,18 +22,19 @@ namespace EjemploFechas
             //declaramos una variable de tipo numero real con decimales
             double costo = 0;
             //preguntamos en un switch que dia es hoy y de esta manera darle el valor del la variable costo
+            //No olvides que los dias tienen acento para iogual al sistema de computadora, á, é, í, ó, ú.
             switch (dia)
             {
                 case "domingo": costo = 2;break;
                 case "lunes":
                 case "martes":
-                case "miercoles":
+                case "miércoles":
                 case "jueves": costo = 4; break;
                 case "viernes":
                 case "sabado":costo = 7; break;
             }
             //mostramos el valor de costo, tomando en cuenta el dia de la semana
-            Console.WriteLine(costo);
+            Console.WriteLine(costo.ToString("C"));
             Console.ReadKey();
         }
     }
